fbba6e08fcc4b1d74bc7068757372ba0592c2a13
Pekka <pekkaliu@gmail.com>
2019-04-23 18:30:00 +0300

    Create puhelinluettelo - app

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+
+const Person = ({person}) => <li>{person.name}  {person.number}</li>  
+
+const Persons = ({persons}) => 
+  <ul>{persons.map(person => <Person key={person.name} person={person} />)}</ul>
+
+const Filter = ({searchWord, handleSearchChange}) => 
+  <p>rajaa näytettäviä: <input value={searchWord} onChange={handleSearchChange} /></p>
+
+const PhonebookForm = ({handleFormSubmit, handleNameChange, handleNumberChange, newName, newNumber}) =>
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        nimi: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>numero: <input value={newNumber} onChange={handleNumberChange} /></div>
+      <div>
+        <button type="submit">lisää</button>
+      </div>
+    </form>
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchWord, setsearchWord ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleSearchChange = (event) => {
+    setsearchWord(event.target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    // Callback 
+    // http://adripofjavascript.com/blog/drips/testing-array-contents-with-array-some.html
+    const isObjInArray = (element) => element.name === newName;
+
+    if(persons.some(isObjInArray)) {
+      alert(`${newName} on jo luettelossa`)
+    } else {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  // Case insensitive filter
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+  const filterCallback = (person) =>  RegExp(searchWord,'i').test(person.name)
+  const personsToShow = persons.filter(filterCallback)
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+
+      <Filter searchWord={searchWord} handleSearchChange={handleSearchChange} />
+
+      <h3>Lisää uusi</h3>
+
+      <PhonebookForm 
+        handleFormSubmit={handleFormSubmit} 
+        handleNameChange={handleNameChange} 
+        handleNumberChange={handleNumberChange} 
+        newName={newName} 
+        newNumbe={newNumber} 
+      />
+
+      <h3>Numerot</h3>
+
+      <Persons persons={personsToShow} />
+    </div>
+  )
+
+}
+
+export default App
